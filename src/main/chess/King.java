0063c063ba1440d6723a278f69d8dfd258dcdcfd
radmuffin <dbas2@byu.edu>
2023-10-04 22:01:56 -0600

    all but pawn moves

--- src/main/chess/King.java
+++ src/main/chess/King.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 public class King extends ChessPieceAb{
 
@@ -11,7 +12,33 @@ public class King extends ChessPieceAb{
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return super.pieceMoves(board, myPosition);
+        HashSet<ChessMove> options = new HashSet<ChessMove>();
+        probe(1,0, myPosition, options, board);     //up
+        probe(1,1, myPosition, options, board);     //up right
+        probe(0,1, myPosition, options, board);     //right
+        probe(-1,1, myPosition, options, board);    //down right
+        probe(-1,0, myPosition, options, board);    //down
+        probe(-1,-1, myPosition, options, board);   //down left
+        probe(0,-1, myPosition, options, board);    //left
+        probe(1,-1, myPosition, options, board);    //up left
+        return options;
+    }
+
+    //adds move per adjust h, v if valid
+    private void probe(int h, int v, ChessPosition start, Collection<ChessMove> stuff, ChessBoard board) {
+        ChessPosition look = start.copy();
+        look.adjust(h, v);
+        if (look.onBoard()) {
+            ChessPiece inSpot = board.getPiece(look);
+            if (inSpot == null) {
+                stuff.add(new ChessMoveImp(start, look, null));
+            }
+            else {
+                if (inSpot.getTeamColor() != team) {
+                    stuff.add(new ChessMoveImp(start, look, null));
+                }
+            }
+        }
     }
 
     @Override
--- src/main/chess/Knight.java
+++ src/main/chess/Knight.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 public class Knight extends ChessPieceAb{
 
@@ -11,7 +12,33 @@ public class Knight extends ChessPieceAb{
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return super.pieceMoves(board, myPosition);
+        HashSet<ChessMove> options = new HashSet<ChessMove>();
+        probe(1,2, myPosition, options, board);     //up right right
+        probe(-1,2, myPosition, options, board);    //down right right
+        probe(-2,1, myPosition, options, board);    //down down right
+        probe(-2,-1, myPosition, options, board);   //down down left
+        probe(-1,-2, myPosition, options, board);   //down left left
+        probe(1,-2, myPosition, options, board);    //up left left
+        probe(2,-1, myPosition, options, board);    //up up left
+        probe(2,1, myPosition, options, board);     //up up right
+        return options;
+    }
+
+    //adds move per adjust h, v if valid
+    private void probe(int h, int v, ChessPosition start, Collection<ChessMove> stuff, ChessBoard board) {
+        ChessPosition look = start.copy();
+        look.adjust(h, v);
+        if (look.onBoard()) {
+            ChessPiece inSpot = board.getPiece(look);
+            if (inSpot == null) {
+                stuff.add(new ChessMoveImp(start, look, null));
+            }
+            else {
+                if (inSpot.getTeamColor() != team) {
+                    stuff.add(new ChessMoveImp(start, look, null));
+                }
+            }
+        }
     }
 
     @Override
