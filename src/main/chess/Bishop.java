b2de0d4e8dbbeef0c1b580f9649123d65922f9ed
radmuffin <dbas2@byu.edu>
2023-10-04 21:44:47 -0600

    bishop and queen moves

--- src/main/chess/Bishop.java
+++ src/main/chess/Bishop.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 public class Bishop extends ChessPieceAb{
 
@@ -11,8 +12,34 @@ public class Bishop extends ChessPieceAb{
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return super.pieceMoves(board, myPosition);
+        HashSet<ChessMove> options = new HashSet<ChessMove>();
+        probe(1,1, myPosition, options, board);     //upper right
+        probe(1,-1, myPosition, options, board);    //upper left
+        probe(-1,1, myPosition, options, board);    //lower right
+        probe(-1,-1, myPosition, options, board);   //lower left
+        return options;
     }
+
+    //adds moves going in direction per adjust h, v
+    private void probe(int h, int v, ChessPosition start, Collection<ChessMove> stuff, ChessBoard board) {
+        ChessPosition look = start.copy();
+        look.adjust(h, v);
+        while (look.onBoard()) {
+            ChessPiece inSpot = board.getPiece(look);
+            if (inSpot == null) {
+                stuff.add(new ChessMoveImp(start, look, null));
+            }
+            else {
+                if (inSpot.getTeamColor() != team) {
+                    stuff.add(new ChessMoveImp(start, look, null));
+                }
+                break;
+            }
+            look = look.copy();
+            look.adjust(h, v);
+        }
+    }
+
 
     @Override
     public String toString() {
